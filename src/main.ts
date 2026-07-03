2e5f9106c4b2401823058d9c61bcd0f9f4b7e846
cargoax38 <rino.sanfilippo@gmail.com>
2026-07-03 23:44:59 +0200

    Premier ajout d'une fonctionnalite
    Creation de la branche (et premier ajouts) d'une premiere fonctionnalite : un gestionnaire automatique des titres d'une page obsidian.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {
+	MarkdownView,
 	Plugin,
+	TFile,
 } from 'obsidian';
 import {
 	DEFAULT_SETTINGS,
@@ -14,6 +16,61 @@ export default class LatexDocument extends Plugin {
 		await this.loadSettings();
 
 		this.addSettingTab(new LatexDocumentSettingTab(this.app, this));
+
+		this.app.workspace.on('quick-preview', () => {
+			console.log("test");
+		})
+
+		this.app.workspace.on('file-open', () => {
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if(!activeView) return;
+
+			setTimeout(() => {
+				const container = activeView.containerEl.querySelector('.markdown-preview-view');
+				if(!container) return;
+
+				const codeBlock = container.querySelectorAll('code');
+
+				codeBlock.forEach((block) => {
+					if(block.textContent.contains('\\test')) {
+						const h1 = activeDocument.createElement('h1');
+						h1.textContent = 'Test';
+
+						block.parentElement?.replaceWith(h1);
+					}
+				});
+			}, 100);
+		});
+
+		this.addCommand({
+			id: 'latex-document-sec',
+			name: 'Sections seeker',
+			callback: () => {
+				//const activeFile = this.app.workspace.getActiveFile() as TFile;
+
+				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if(!activeView) return;
+
+				setTimeout(() => {
+					const container = activeView.containerEl.querySelector('.markdown-preview-view');
+					if(!container) return;
+
+					const codeBlock = container.querySelectorAll('code');
+
+					codeBlock.forEach((block) => {
+						if(block.textContent.contains('\\test')) {
+							const h1 = activeDocument.createElement('h1');
+							h1.textContent = 'Test';
+
+							block.parentElement?.replaceWith(h1);
+						}
+					});
+				}, 100);
+
+				/*this.app.vault.read(activeFile).then((contenuFichier) => {
+					console.log("Contenu du fichier :", contenuFichier);
+				});*/
+			}});
 	}
 
 	onunload() {}
